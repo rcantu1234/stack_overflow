462cece8b3b429e7ba88b59c964c0083ad23b16a
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-03 14:10:44 -0600

    Editing

--- db/migrate/20150203193522_create_answers.rb
+++ db/migrate/20150203193522_create_answers.rb
@@ -1,6 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
+      t.belongs_to :question, index: true
       t.string :text
 
       t.timestamps null: false
