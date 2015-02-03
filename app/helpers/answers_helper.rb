1e35a3e4debcffb8f15a8d7aa4e3ca7d3962c6dd
ROEL CANTU <roel.cantu@yahoo.com>
2015-02-03 15:17:31 -0600

    Editing

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,5 @@
 module AnswersHelper
+  def question_collection
+      Question.select('id', 'title')
+  end
 end
--- db/migrate/20150203193522_create_answers.rb
+++ db/migrate/20150203193522_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-      # t.belongs_to :question, index: true
+      t.belongs_to :question, index: true
       t.string :text
 
       t.timestamps null: false
